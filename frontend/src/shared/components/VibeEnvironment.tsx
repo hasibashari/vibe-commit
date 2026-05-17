0304f5b90c5584cddd68c63d59c67d95f474cc03
Hasib Ashari <hasibashari@gmail.com>
2026-05-17 17:54:47 +0700

    refactor: remove unused useEffect import from VibeEnvironment component

--- frontend/src/shared/components/VibeEnvironment.tsx
+++ frontend/src/shared/components/VibeEnvironment.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { motion } from 'motion/react';
 import { useMouseParallax } from '../hooks/useMouseParallax';
 import { getWeatherState } from '../utils/weatherUtils';
