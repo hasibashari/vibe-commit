073d14f8bb10cec9e0b6d817507037b6c51479a1
Hasib Ashari <hasibashari@gmail.com>
2026-05-31 12:03:15 +0700

    chore: update gitignore and refresh service worker cache revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.n7aaj1amtsc"
+    "revision": "0.ia0on6l9pms"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
